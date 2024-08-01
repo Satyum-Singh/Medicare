f73751678eeb682fcfcab452b279cf98b59a624c
Satyum <satyumspathania@gmail.com>
2024-08-01 12:48:51 +0530

    completed testimonials section.

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,100 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import { RiLinkedinFill } from 'react-icons/ri';
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
+
+const socialLinks = [
+  {
+    path: "/",
+    icon: <AiFillYoutube className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "https://github.com/Satyum-Singh",
+    icon: <AiFillGithub className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "/",
+    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "https://www.linkedin.com/in/satyum-singh-pathania/",
+    icon: <RiLinkedinFill className='group-hover:text-white w-4 h-5' />,
+  }
+]
+
+const quickLinks01 = [
+  {
+    path: '/home',
+    display: "Home",
+  },
+  {
+    path: '/',
+    display: "About Us",
+  },
+  {
+    path: '/services',
+    display: "Services",
+  },
+  {
+    path: '/',
+    display: "Blog",
+  },
+]
+
+const quickLinks02 = [
+  {
+    path: '/find-a-doctor',
+    display: "Find a Doctor",
+  },
+  {
+    path: '/',
+    display: "Request an Appointment",
+  },
+  {
+    path: '/',
+    display: "Find a Location",
+  },
+  {
+    path: '/',
+    display: "Get a Opinion",
+  }
+]
+
+const quickLinks03 = [
+  {
+    path: '/',
+    display: "Donate",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  }
+]
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+              Copyright © {year} developed by Muhibur Rahman all right reserved.
+            </p>
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((link, index) =>
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group  hover:bg-primaryColor hover:border-none">
+                  {link.icon}
+                </Link>
+)}
+          </div>
+        </div>
+      </div>
+    </div>
+</footer >
   )
 }
 
