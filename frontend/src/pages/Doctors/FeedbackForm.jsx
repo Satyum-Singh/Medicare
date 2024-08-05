64f374eb1476f79b19e66b1fd734c015f3d26fd6
Satyum <satyumspathania@gmail.com>
2024-08-05 23:13:02 +0530

    working on feedback form.

---
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AiFillStar } from 'react-icons/ai';
+
+const FeedbackForm = () => {
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+
+    return (
+        <form action="">
+            <div>
+                <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+                    How would you rate the overall experience?*
+                </h3>
+                <div>
+                    {[...Array(5).keys()].map((_, index) => { 
+                    index += 1;
+                    return (
+                    <button key={index} type="button" onMouseEnter={() => setRating(index)} onMouseLeave={() => setRating(0)}>
+                        <span>
+                            <AiFillStar color={index <= (hover || rating) ? "#0067FF" : "#BDBDBD"} />
+                        </span>
+                    </button>
+                    );
+                    })}
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default FeedbackForm
