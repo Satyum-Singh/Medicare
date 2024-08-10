c25e35b78c3e68c9b4ce0025c31b0fb9e6d61e24
Satyum <satyumspathania@gmail.com>
2024-08-11 00:53:00 +0530

    completed login page.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 
-    const [formData,setFormData] = useState({
-        email:"",
-        password:""
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
     })
 
-    const handleInputChange = (e) =>{
-        setFormData({...formData,[e.target.name]:e.target.value})
+    const handleInputChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     return (
@@ -18,17 +19,25 @@ const Login = () => {
                 <form action="" className="py-4 md:py-0">
                     <div className="mb-5">
                         <input type="email" placeholder="Enter Your Email" name='email' value={formData.email} onChange={handleInputChange}
-                            className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                            className="w-full px-4 py-3 border-b  border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
                             required
                         />
                     </div>
 
                     <div className="mb-5">
-                        <input type="password" placeholder="Enter Your Email" name='password' value={formData.password} onChange={handleInputChange}
-                            className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                        <input type="password" placeholder="Password" name='password' value={formData.password} onChange={handleInputChange}
+                            className="w-full px-4 py-3 border-b  border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
                             required
                         />
                     </div>
+
+                    <div className="mt-7">
+                        <button type="submit" className="w-full bg-primaryColor text-white text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 ">Login</button>
+                    </div>
+
+                    <p className="mt-5 text-center text-textColor">
+                        Don&apos;t have an account <Link to='/register' className="text-primaryColor font-medium ml-1">Register</Link>
+                    </p>
                 </form>
             </div>
         </section>
