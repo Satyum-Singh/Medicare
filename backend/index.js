ce7eb0b8376f8f8a6f73d1178566361983f354d7
Satyum <satyumspathania@gmail.com>
2024-08-12 11:40:49 +0530

    made a connection with database.

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true, // it allows any domain to access the server which is helpfull in the development process.
+};
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+// database connect
+mongoose.set("strictQuery", false); //strictQuery is particularily used for accidental data losses while sending data of database that dont exactly match the schema
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true, //The new parser can handle more complex MongoDB connection strings, including those with multiple hosts, replica sets, and authentication. The new parser provides more informative error messages when there's an issue with the connection string
+      useUnifiedTopology: true, // used for improved connection management by providing a single, unified way to handle connections to MongoDB by reducing number of connections.
+    });
+    console.log("MongoDB database is connected");
+  } catch (err) {
+    console.log(err);
+    console.log("Connection failed");
+  }
+};
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser()); // used to parse cookie coming with req or send cookie with a response as res.cookie() method.
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on port ${port}`);
+});
