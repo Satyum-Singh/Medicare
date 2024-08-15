a538b8428403830d91c769874cf5e33f7c86cf41
Satyum <satyumspathania@gmail.com>
2024-08-15 16:27:49 +0530

    created authorization for login.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         <section className="px-5 lg:px-0">
             <div className="max-w-[570px] md:p-10 w-full mx-auto rounded-lg shadow-md">
                 <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">Hello! <span className="text-primaryColor">Welcome</span> Back 👋😃</h3>
-                <form action="" className="py-4 md:py-0">
+                <form action="post" className="py-4 md:py-0">
                     <div className="mb-5">
                         <input type="email" placeholder="Enter Your Email" name='email' value={formData.email} onChange={handleInputChange}
                             className="w-full px-4 py-3 border-b  border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
