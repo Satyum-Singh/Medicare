2be81791ea2d5a36e2664afd972a4fea5fc1c5ef
Satyum <satyumspathania@gmail.com>
2024-09-18 17:37:03 +0530

    created customhook for fetching userData.

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,45 +1,66 @@
-import { useContext } from 'react'
-import { authContext } from './../../context/AuthContext' 
+import { useContext, useState } from 'react'
+import { authContext } from './../../context/AuthContext'
 import userImg from '../../assets/images/doctor-img01.png'
+import MyBookings from './MyBookings'
+import Profile from './Profile'
 
 const MyAccount = () => {
 
   const { dispatch } = useContext(authContext);
+  const [tab, setTab] = useState('bookings')
+
   const handleLogout = () => {
     dispatch({ type: 'LOGOUT' });
   }
 
   return (
-    <div className="max-w-[1170px] px-5 mx-auto">
-      <div className="grid md:grid-cols-3 gap-10">
-        <div className="pb-[50px] px-[30px] rounded-md ">
-          <div className="flex items-center justify-center">
-            <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
-              <img src={userImg} alt="" className='w-full h-full rounded-full' />
-            </figure>
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        <div className="grid md:grid-cols-3 gap-10">
+          <div className="pb-[50px] px-[30px] rounded-md ">
+            <div className="flex items-center justify-center">
+              <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
+                <img src={userImg} alt="" className='w-full h-full rounded-full' />
+              </figure>
+            </div>
+
+            <div className='text-center mt-4'>
+              <h3 className="text-headingColor font-bold text-[18px] leading-[30px]">
+                Ayush Katoch
+              </h3>
+              <p className="text-textColor leading-6 font-medium text-[15px] ">
+                example@gmail.com
+              </p>
+              <p className="text-textColor leading-6 font-medium ">
+                Blood Type:
+                <span className="ml-2 text-headingColor leading-8 text-[22px] ">O-</span>
+              </p>
+            </div>
+
+            <div className="mt-[50px] md:mt-[100px]">
+              <button onClick={handleLogout} className="w-full p-3 leading-7 text-[16px] bg-[#181a1e] text-white rounded-md">Logout</button>
+              <button className="w-full mt-4 p-3 leading-7 text-[16px] bg-red-600 text-white rounded-md">Delete Account</button>
+            </div>
+
           </div>
 
-          <div className='text-center mt-4'>
-            <h3 className="text-headingColor font-bold text-[18px] leading-[30px]">
-              Ayush Katoch
-            </h3>
-            <p className="text-textColor leading-6 font-medium text-[15px] ">
-              example@gmail.com
-            </p>
-            <p className="text-textColor leading-6 font-medium ">
-              Blood Type:
-              <span className="ml-2 text-headingColor leading-8 text-[22px] ">O-</span>
-            </p>
-          </div>
+          <div className="md:col-span-2 md:px-[30px]">
+            <div>
+              <button onClick={() => setTab('bookings')} className={` ${tab === 'bookings' && "bg-primaryColor text-white font-normal"} p-2 mr-2 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
+                My Bookings
+              </button>
+              <button onClick={() => setTab('settings')} className={` ${tab === 'settings' && "bg-primaryColor text-white font-normal"} p-2 mr-2 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}>
+                Profile Settings
+              </button>
+            </div>
 
-          <div className="mt-[50px] md:mt-[100px]">
-            <button onClick={handleLogout} className="w-full p-3 leading-7 text-[16px] bg-[#181a1e] text-white rounded-md">Logout</button>
-            <button className="w-full mt-4 p-3 leading-7 text-[16px] bg-red-600 text-white rounded-md">Delete Account</button>
-          </div>
+            {tab === 'bookings' && <MyBookings />}
+            {tab === 'settings' && <Profile />}
 
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
---
+++ frontend/src/hooks/useFetchData.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { token } from '../config.js'
+
+const useFetchData = () => {
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(url, {
+                    headers: { Authorization: `Bearer ${token}` }
+                })
+                const result = await res.json()
+                if(!res.ok){
+                    throw new Error(result.message+"🤢")
+                }
+
+                setData(result.data)
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+                setError(error.message)
+            }
+        }
+
+        fetchData()
+    }, [url])
+
+    return {
+        data,loading,error,
+    }
+}
+
+export default useFetchData
