a25cdc23df971528503f5b9f46f70fd1e2d32f6f
Satyum <satyumspathania@gmail.com>
2024-10-04 19:33:22 +0530

    this profile will take years.

--- frontend/src/Dashboard/doctor-account/Profile.jsx
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -13,7 +13,8 @@ const Profile = () => {
         ticketPrice: 0,
         qualifications: [{ startingDate: "", endingDate: "", degree: "", university: "" }],
         experiences: [{ startingDate: "", endingDate: "", position: "", hospital: "" }],
-        timeSlots: [],
+        timeSlots: [{ day: "", startingTime: "", endingTime: "" }],
+        about: ''
     })
 
     const handleInputChange = e => {
@@ -204,54 +205,59 @@ const Profile = () => {
                         <div>
                             <div className='grid grid-cols-2 md:grid-cols-4 mb-[30px] gap-5'>
                                 <div>
-                                    <p className="form__label">Starting Date*</p>
+                                    <p className="form__label">Day*</p>
+                                    <select name="day" value={item.day} className='form__input py-3.5'>
+                                        <option value="">Select</option>
+                                        <option value="saturday">Saturday</option>
+                                        <option value="sunday">Sunday</option>
+                                        <option value="monday">Monday</option>
+                                        <option value="tuesday">Tuesday</option>
+                                        <option value="wednesday">Wednesday</option>
+                                        <option value="thursday">Thursday</option>
+                                        <option value="friday">Friday</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <p className="form__label">Starting Time*</p>
                                     <input
-                                        type="date"
-                                        name="startingDate"
-                                        value={item.startingDate}
+                                        type="time"
+                                        name="startingTime"
+                                        value={item.startingTime}
                                         className="form__input"
                                     />
                                 </div>
                                 <div>
-                                    <p className="form__label">Ending Date*</p>
+                                    <p className="form__label">Ending Time*</p>
                                     <input
-                                        type="date"
-                                        name="endingDate"
-                                        value={item.endingDate}
+                                        type="time"
+                                        name="endingTime"
+                                        value={item.endingTime}
                                         className="form__input"
                                     />
+                                </div>
+
+                                <div className='flex items-center'>
+                                    <button className='bg-red-600 p-2 rounded-full text-white text-[18px] cursor-pointer mt-6'>
+                                        <AiOutlineDelete />
+                                    </button>
                                 </div>
                             </div>
-
-                            <div className='grid grid-cols-2 gap-5 mt-5'>
-                                <div>
-                                    <p className="form__label">Position*</p>
-                                    <input
-                                        type="text"
-                                        name="position"
-                                        value={item.position}
-                                        className="form__input"
-                                    />
-                                </div>
-                                <div>
-                                    <p className="form__label">Hospital*</p>
-                                    <input
-                                        type="text"
-                                        name="hospital"
-                                        value={item.hospital}
-                                        className="form__input"
-                                    />
-                                </div>
-                            </div>
-
-                            <button className='bg-red-600 p-2 rounded-full text-white text-[18px] mt-2 mb-[30px] cursor-pointer'>
-                                <AiOutlineDelete />
-                            </button>
                         </div>
                     </div>
                     )}
 
-                    <button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">Add Experience</button>
+                    <button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+                        Add TimeSlot
+                    </button>
+                </div>
+
+                <div className='mb-5'>
+                    <p className='form__label'>About*</p>
+                    <textarea name="about" rows={5} value={formData.about} placeholder='Write about you'
+                        onChange={handleInputChange}
+                        className='form__input'
+                    >
+                    </textarea>
                 </div>
             </form>
 
