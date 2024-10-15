5705a427b1230e29c7f56b1b83831ba840f52760
Satyum <satyumspathania@gmail.com>
2024-10-15 17:41:19 +0530

    finished with debounce function in search doctor.

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -40,7 +40,8 @@ const DoctorCard = ({ doctor }) => {
                         At {experiences && experiences[0]?.hospital}
                     </p>
                 </div>
-                <Link to="/doctors" className='w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e]  flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                <Link to={`/doctors/${doctor._id}`}
+                    className='w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e]  flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
                     <BsArrowRight className='group-hover:text-white w-6 h-5' />
                 </Link>
             </div>
--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,27 +1,57 @@
 import DoctorCard from '../../components/Doctors/DoctorCard'
 import { doctors } from '../../assets/data/doctors'
 import Testimonial from '../../components/Testimonial/Testimonial'
+import { BASE_URL } from './../../config'
+import useFetchData from '../../hooks/useFetchData';
+import Loader from '../../components/Loader/Loading';
+import Error from '../../components/Error/Error';
+import { useState, useEffect } from 'react';
 
 const Doctors = () => {
+    const [query, setQuery] = useState('')
+    const [debounceQuery, setDebounceQuery] = useState("");
+
+    const handleSearch = () => {
+        setQuery(query.trim())
+        console.log('handle search');
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDebounceQuery(query)
+        }, 1000)
+        return () => clearTimeout(timeout)
+    }, [query])
+
+    const { data: doctors, loading, error } = useFetchData(`${BASE_URL}/doctors?query=${debounceQuery}`)
+
     return (
         <>
             <section className='bg-[#fff9ea]'>
                 <div className="container text-center">
                     <h2 className="heading"></h2>
                     <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff1c] rounded-md flex items-center justify-between">
-                        <input type="search" placeholder='Search Doctor here..' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' />
-                        <button className='btn mt-0 rounded-[0px] rounded-r-md ' >Search</button>
+                        <input
+                            type="search"
+                            placeholder='Search doctor by name or specialization ..'
+                            className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor'
+                            value={query}
+                            onChange={e => setQuery(e.target.value)}
+                        />
+                        <button className='btn mt-0 rounded-[0px] rounded-r-md' onClick={handleSearch} >Search</button>
                     </div>
                 </div>
             </section>
 
             <section>
                 <div className="container">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                    {loading && <Loader />}
+                    {error && <Error />}
+                    {!loading && !error && (<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                         {doctors.map((doctor) => (
                             <DoctorCard key={doctor.id} doctor={doctor} />
                         ))}
-                    </div>
+                    </div>)}
                 </div>
             </section>
 
